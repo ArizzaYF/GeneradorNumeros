54e4eca0e63051d408699bf6698a2303020f6fb9
Jairo Ariza <jairoaariza@uts.edu.co>
2023-06-29 16:50:24 -0500

    Se agrego el footer respectivo, cambiaron las cifras de los numeros generados y se ajusto el diseño responsivo

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 window.onload = function() {
-    var generateBtn = document.getElementById('generateBtn');
-    var numberContainer = document.getElementById('numberContainer');
-  
-    generateBtn.addEventListener('click', function() {
-      var numDigits = Math.floor(Math.random() * 3) + 2; // Genera un número aleatorio entre 2 y 4
-      var randomNumber = generateRandomNumber(numDigits);
-      numberContainer.textContent = randomNumber;
-    });
-  
-    function generateRandomNumber(numDigits) {
-      var min = Math.pow(10, numDigits - 1);
-      var max = Math.pow(10, numDigits) - 1;
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-  };
+  var generateBtn = document.getElementById('generateBtn');
+  var numberContainer = document.getElementById('numberContainer');
+
+  generateBtn.addEventListener('click', function() {
+    var numDigits = Math.floor(Math.random() * 2) + 3; // Genera un número aleatorio entre 3 y 4
+    var randomNumber = generateRandomNumber(numDigits);
+    numberContainer.textContent = randomNumber;
+  });
+
+  function generateRandomNumber(numDigits) {
+    var min = Math.pow(10, numDigits - 1);
+    var max = Math.pow(10, numDigits) - 1;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+};
+
   
